8d9716b9348916a24076b974a1577c2ed3ca93bd
dawn <zsc_1992@163.com>
2016-02-29 00:28:04 +0800

    simplify the js

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,47 +1,27 @@
-
-
 var $img = $('#image');
 $img.addClass('blur');
 var value;
-var $blur = $('#blur'),
-    $brightness = $('#brightness'),
-    $grayscale = $('#grayscale'),
-    $opacity = $('#opacity'),
-    $saturate = $('#saturate');
-
+var pattern;
 $(function() {
-    $blur.bind('change', function() {
-        // slide('blur',$(this));
-        var str = $img.css("-webkit-filter");
-        var value ='blur(' + $blur.val() + 'px)';
-        if (str.match(/blur\(\d+px\)/)) {
-            str = str.replace(/blur\(\d+px\)/,value);
-        }
-        else {
-            str += str.length > 0 ? ' ' + value : value;
-        }
-        $img.css("-webkit-filter", str);
-    })
-    
-    $brightness.bind('change', function() {
-        slide('brightness',$(this));
-    })
-    $grayscale.bind('change', function() {
-        slide('grayscale',$(this));
-    })
-    $opacity.bind('change', function() {
-        slide('opacity',$(this));
-    })
-    $saturate.bind('change', function() {
-        slide('saturate',$(this));
-    })
-
+    //获取所有滑动条
+    var $input = $('.slide-wrap input');
+    for (var i = 0, len =  $input.length; i < len; i++ ) {
+        $($input[i]).bind('change', function() {
+            slide(this.id, $(this));
+        });
+    }
 });
+//根据滑动条的值设置图像的css
 function slide(id, obj) {
     var str = $img.css("-webkit-filter");
-    var value = id + '(' + obj.val() + ')';
-    console.log(str);
-    var pattern = new RegExp(id + "\\(\\d+\\.\?\\d*\\)");
+    if (id === 'blur') {
+        value = id + '(' + obj.val() + 'px)';
+        pattern = new RegExp(id + "\\(\\d+px\\)");
+    }
+    else {
+        value = id + '(' + obj.val() + ')';
+        pattern = new RegExp(id + "\\(\\d+\\.\?\\d*\\)");
+    }
     if (pattern.test(str)) {
         str = str.replace(pattern,value);
     }
