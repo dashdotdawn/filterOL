c2e06c97a550bfd602b714e48a00fb46bf656e62
dawn <zsc_1992@163.com>
2016-02-29 15:55:40 +0800

    adjust color

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,17 +1,17 @@
-var $img = $('#image');
-$img.addClass('blur');
-
-//适应移动屏幕
-var width = window.innerWidth;
-var height = window.innerHeight;
-
-var posl = (width - $img.width()) / 2,
-    post = (height - $img.height()) / 2;
-$('.adapt').width(width).height(height);
-$img.css({'left': posl + 'px', 'top': post + 'px'});
-
-
 $(function() {
+    var $img = $('#image');
+    $img.addClass('blur');
+
+    //适应移动屏幕
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    //若未设置img尺寸，则需要绑定load事件
+    var posl = (width - $img.width()) / 2,
+        post = (height - $img.height()) / 2;
+    $('.adapt').width(width).height(height);
+    $img.css({'left': posl + 'px', 'top': post + 'px'});
+
     //获取所有滑动条
     var $input = $('#menu input');
     for (var i = 0, len =  $input.length; i < len; i++ ) {
@@ -21,10 +21,10 @@ $(function() {
     }
 });
 
-var value;
-var pattern;
 //根据滑动条的值设置图像的css
 function slide(id, obj) {
+    var value;
+    var pattern;
     var str = $img.css("-webkit-filter");
     if (id === 'blur') {
         value = id + '(' + obj.val() + 'px)';
